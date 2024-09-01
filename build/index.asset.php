eb5a8a438f079af10587ba919c6455918822c007
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-09-01 18:59:37 -0400

    Clean up

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fff8beb26f1f968951f9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f792490c7243c3ceae59');
