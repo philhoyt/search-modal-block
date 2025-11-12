498a19c8f60331466a63f57c2d4738cd7ecedb36
Phil Hoyt <philhoyt@users.noreply.github.com>
2025-11-12 11:05:31 -0500

    Accessibility issue

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fc5cf9595723574bc2b8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4083f02593e957aec073');
