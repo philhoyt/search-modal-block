084f24228a43a8c89fddd02664b89974ad62dfaf
Phil Hoyt <philhoyt@users.noreply.github.com>
2025-11-12 11:09:18 -0500

    Update buttons to use variant

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4083f02593e957aec073');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '52a3876be09813c2ddb6');
